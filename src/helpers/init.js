117cc23e2b0d2f215ca677f8d59267318f348d20
Edudotnexx <Edoudotnexx@outlook.com>
2024-11-27 12:34:10 +0330

    Update init.js

--- src/helpers/init.js
+++ src/helpers/init.js
@@ -1,12 +1,12 @@
 import { isValidUUID } from './helpers';
-const defaultProxyIP = 'bpb.yousef.isegaro.com';
+const defaultProxyIP = 'proxyip-germany.cip.web1337.net	';
 let userID, dohURL, proxyIP, trojanPassword, defaultHttpPorts, defaultHttpsPorts, panelVersion, hostName, origin, client, pathName;
 
 function initParams(request, env) {
     const proxyIPs = env.PROXYIP?.split(',').map(proxyIP => proxyIP.trim());
-    userID = env.UUID || '89b3cbba-e6ac-485a-9481-976a0415eab9';
+    userID = env.UUID || '3b8512ae-6a33-45b9-a66c-fd6323d57b4c';
     if (!isValidUUID(userID)) throw new Error(`Invalid UUID: ${userID}`);
-    dohURL = env.DOH_URL || 'https://cloudflare-dns.com/dns-query';
+    dohURL = env.DOH_URL || 'https://sky.rethinkdns.com/dns-query';
     proxyIP = proxyIPs ? proxyIPs[Math.floor(Math.random() * proxyIPs.length)] : defaultProxyIP;
     trojanPassword = env.TROJAN_PASS || 'bpb-trojan';
     defaultHttpPorts = ['80', '8080', '2052', '2082', '2086', '2095', '8880'];
